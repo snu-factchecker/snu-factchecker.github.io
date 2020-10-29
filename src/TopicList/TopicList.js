67430d8def314da04b91087976b75393bf4734af
Heather Kim <haesoo1108@gmail.com>
2020-10-29 17:38:25 +0900

    add numbers to list

--- src/TopicList/TopicList.js
+++ src/TopicList/TopicList.js
@@ -6,19 +6,19 @@ import "./Topic.scss"
 class TopicList extends React.Component{
 	state = {
 		topics: [{
-			text: "국가행사",
+			text: "1. 국가행사",
 			link: 1,
 		},{
-			text: "사회/IT과학",
+			text: "2. 사회/IT과학",
 			link: 3
 		},{
-			text: "IT과학 분야 정치인 발언",
+			text: "3. IT과학 분야 정치인 발언",
 			link: 4
 		},{
-			text: "국제/환경",
+			text: "4. 국제/환경",
 			link: 5
 		},{
-			text: "사회/경제",
+			text: "5. 사회/경제",
 			link: 6
 		}]
 	}
@@ -31,7 +31,8 @@ class TopicList extends React.Component{
         });
 
 		return(<div id="topic">
-			<div>팩트체크를 진행할 주제를 골라 주세요.</div>
+			<div>총 다섯개 주제에 대한 팩트체크 게임을 시작합니다. <br/>
+			1부터 5까지 순서대로 클릭해 플레이하세요. 모두 완료 후 설문에 응해주시면 게임 플레이가 완료됩니다.</div>
 			<div id="topic-list">
 				{topics}
 			</div>
