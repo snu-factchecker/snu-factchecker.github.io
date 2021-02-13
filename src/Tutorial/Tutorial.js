8f8b91e789fe87659a8e1f1d2bc003c90b0f41b5
Heather Kim <haesoo1108@gmail.com>
2021-02-13 17:11:58 +0900

    Add tutorial skip

--- src/Tutorial/Tutorial.js
+++ src/Tutorial/Tutorial.js
@@ -166,10 +166,7 @@ class Tutorial extends React.Component{
 						<div className="item-preview">“우리 경제 신성장 동력 마련 / 분단 극복할 때 광복도 완성” / “日 부당한 수출규제에 맞서 / 뚜벅뚜벅 경제강국 길 갈 것” / 2020 도쿄올림픽 성원 보내 / 한·일 갈등 외교적 해결 방점</div>
 						<div className="sourceBox">
 							<div className="item-source">세계일보 |</div>
-							<Clue innerText="2019.08.15" tooltip="마스크를 쓰지 않은 사진은 2019년 행사 사진이네?" className="item-date" onClick={()=>{
-								this.onClickClue("지난해 행사 사진");
-								this.setFactScore(-10);
-							}}></Clue>
+							<div className="item-date">2019.08.15</div>
 						</div>
 						<div className={`item-content ${this.state.visibility[0]?"":"invisible"}`}>
 						<p>문재인 대통령은 15일 “일본의 부당한 수출규제에 맞서 우리는 책임 있는 경제강국을 향한 길을 뚜벅뚜벅 걸어갈 것”이라며 주요 부품·소재의 국산화와 수입 다변화를 통한 ‘극일(克日)’ 의지를 분명히 했다.문 대통령은 이날 충남 천안 독립기념관에서 열린 광복절 경축식 경축사에서 “국제 분업체계 속에서 어느 나라든 자국이 우위에 있는 부문을 무기화한다면 평화로운 자유무역 질서가 깨질 수밖에 없다”며 “먼저 성장한 나라가 뒤따라 성장하는 나라의 사다리를 걷어차서는 안 된다”고 지적했다. </p> 그러나 동시에 “지금이라도 일본이 대화와 협력의 길로 나온다면 우리는 기꺼이 손을 잡을 것”이라며 “공정하게 교역하고 협력하는 동아시아를 함께 만들어갈 것”이라고 강조했다. 문 대통령은 “세계인들이 평창에서 평화의 한반도를 보았듯이 도쿄 올림픽에서 우호와 협력의 희망을 갖게 되길 바란다”며 2020년 도쿄 올림픽에 대한 성원도 보냈다. 이 같은 발언들은 최악의 상황으로 치닫고 있는 한·일 갈등에 대한 외교적 해결을 위해 대화의 문을 열어놓으면서 유화적 메시지를 보낸 것으로 풀이된다. <p> 문 대통령은 또 “평화로 번영을 이루는 평화경제를 구축하고 통일로 광복을 완성하고자 한다”며 “평화경제에 우리가 가진 모든 것을 쏟아부어 ‘새로운 한반도’의 문을 활짝 열겠다. 평화경제를 통해 우리 경제의 신성장동력을 만들겠다”고 밝혔다. 그러면서 “‘분단’을 극복해낼 때 비로소 우리의 ‘광복’은 완성되고 아무도 흔들 수 없는 나라가 될 것”이라고 말했다. </p> 남북 평화경제가 실현되면 8000만 단일 시장을 가진 세계 6위권 경제대국이 될 수 있다는 전망도 제시했다. 문 대통령은 특히 “평화경제는 한반도의 완전한 비핵화 위에 북한이 핵이 아닌 경제와 번영을 선택할 수 있도록 대화와 협력을 계속해 나가는 데서 시작한다”며 ‘비핵화를 전제로 한 평화경제’ 개념을 규정해 주목됐다. 최근 북한의 잇단 미사일 도발로 인해 평화경제론에 대한 부정적 의견이 나오는 점을 의식한 듯 문 대통령은 “미국이 북한과 동요 없이 대화를 계속하고 일본 역시 북한과 대화를 추진하고 있는 현실을 직시하기 바란다”며 “이념에 사로잡힌 외톨이로 남지 않길 바란다”고 반박했다. 평화경제를 비난한 자유한국당 등 야당 일각을 ‘이념에 사로잡힌 외톨이’라고 몰아세운 것이다. <p> 문 대통령은 경축사 초반 김기림의 ‘새나라 송(頌)’에 나오는 ‘아무도 흔들 수 없는 나라’를 인용하며 “‘아무도 흔들 수 없는 나라’는 외세의 침략과 지배에서 벗어난 신생독립국가가 가져야 할 당연한 꿈이었지만 아직 이루지 못했다. 아직도 우리가 충분히 강하지 않기 때문이며 아직도 우리가 분단되어 있기 때문”이라고 설명했다. 이어 “저는 오늘 어떤 위기에도 의연하게 대처해온 국민들을 떠올리며 우리가 만들고 싶은 나라, ‘아무도 흔들 수 없는 나라’를 다시 다짐한다”며 이를 위해 △경제강국 △교량국가 △평화경제 구축이라는 3대 목표를 제시했다. 문 대통령은 “경제에서 주권이 확고할 때 우리는 우리 운명의 주인으로 흔들리지 않는다”며 “우리가 힘을 가지면 대륙과 해양을 잇는 나라, 동북아 평화와 번영의 질서를 선도하는 나라가 될 수 있다”고 역설했다.</p>  <p> 김달중 기자 dal@segye.com </p>
@@ -219,6 +216,7 @@ switch(this.state.tutorialStep){
 	case 0:
 		overlay = <div>
 			<div className="overlay-innertext">팩트체크 센터에 오신 것을 환영합니다. 지금부터 여러분은 6개의 주제에 걸쳐 특정 글이나 주장에 대한 근거를 탐색하고, 그 결과를 바탕으로 팩트 여부를 검증하는 과정을 거칠 것입니다.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 			<button className="overlay-nextButton" onClick={()=>this.proceedTutorial()}>다음 단계</button>
 		</div>
 		break;
@@ -229,16 +227,17 @@ switch(this.state.tutorialStep){
 				<div id="quiz">
 					<div id="quiz-header"></div>
 					<div className="quizContent" id="quiz1">
-						<div id="quizitem-title">[일반] 문재인 등 <Clue innerText="8.15 행사" tooltip="올해 광복절 행사에서 진행된 일인가 보네." onClick={() => this.onClickKeyword("2020년 광복절 행사")}/>에서 노마스크</div>
-						<div id="quizitem-author"><Clue innerText="101세형 (181.70)" tooltip="누군지 알 수 없는 오픈 커뮤니티 이용자의 글? " onClick={() => this.onClickClue("신뢰할 수 없는 작성자")}/></div>
+						<div id="quizitem-title">[일반] 문재인 등 8.15 행사에서 노마스크</div>
+						<div id="quizitem-author">101세형 (181.70)</div>
 						<img id="quizitem-image" src={quiz1} alt="article supplement"/>
 						<div id="quizitem-content">저기 실내죠? <br/>
-						<Clue innerText="실내에서 다닥다닥 붙어서 노 마스크로 미친거죠ㅎㅎ" tooltip="자극적인 메시지.. 무슨 의도가 담겨있는 걸까?" onClick={() => this.onClickClue("자극적인 메시지")}/></div>
-						<Clue id="source" tooltip="신뢰하기 쉽지 않은 커뮤니티 게시글이네?" innerText="출처: DC인사이드 우한 마이너 갤러리" onClick={() => this.onClickClue("오픈 커뮤니티 출처")}/>
+						실내에서 다닥다닥 붙어서 노 마스크로 미친거죠ㅎㅎ</div>
+						<div id="source">출처: DC인사이드 우한 마이너 갤러리</div>
 					</div>
 				</div>
 			</div>
 			<div className="overlay-innertext">첫 단계에서는 팩트체크할 내용을 관찰하고, 여기에서 힌트를 찾게 됩니다. 중앙 왼쪽에 있는 칸에는 팩트체크의 대상이 되는 내용이 제공되어 있습니다. 내용을 잘 살펴보고, 중요한 키워드나 팩트체크를 하는 데 있어 힌트가 될 만한 내용을 찾아내야 합니다.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 			<button className="overlay-nextButton" onClick={()=>this.proceedTutorial()}>다음 단계</button>
 		</div>
 		break;
@@ -259,7 +258,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">단서에는 두 가지가 있습니다. 첫 번째는 전체적인 주제를 아우르는 '키워드'와, 두 번째는 내용의 신뢰도를 판단하는 근거가 되는 '정보의 특징'입니다. 우선 키워드를 찾아 보도록 합시다. 아래 글에서 "8.15 행사"라는 텍스트에 마우스를 올려 클릭해보세요.</div>
-			
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 		</div>
 		break;
 	case 3:
@@ -295,6 +294,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">키워드를 찾아 그 위에 마우스를 올리면 관련 내용에 대한 단서가 뜨고, 이를 클릭하면 왼쪽 탐사 노트의 '키워드'란에 기록됩니다. 주제가 되는 키워드를 반드시 찾아야 이후 단계의 팩트체크를 진행할 수 있으니 이 부분을 반드시 유의해 주세요.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 			<button className="overlay-nextButton" onClick={()=>this.proceedTutorial()}>다음 단계</button>
 		</div>
 		break;
@@ -331,6 +331,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">이번에는 다른 단서들을 찾아 봅시다. 팩트체크할 자료의 '출처' 부분에 마우스를 올린 후, 클릭해보세요.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 		</div>
 		break;
 	case 5:
@@ -366,6 +367,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">키워드와 마찬가지로, 정보의 특징 또한 찾을 때마다 탐사 노트에 기록됩니다. 이 정보들을 바탕으로 최종 단계에서 팩트 여부를 검증하게 됩니다. 충분히 키워드를 찾았다면, 후에는, 아래의 '다음 단계' 버튼을 눌러 다음 단계로 넘어갑니다.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 			<button className="overlay-nextButton" onClick={()=>{this.proceedTutorial(); this.proceedStep()}}>다음 단계</button>
 		</div>
 		break;
@@ -404,6 +406,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">두 번째 단계에서는 앞서 찾은 키워드들을 바탕으로 추가적인 검색과 검증을 진행하게 됩니다. 키워드 리스트에 있는 단어를 클릭하면, 화면 중앙의 검색 창에서 관련 자료를 검색할 수 있습니다. 키워드를 클릭해보세요. </div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 		</div>
 		break;
 	case 7:
@@ -444,6 +447,7 @@ switch(this.state.tutorialStep){
 				</div>
 			</div>
 			<div className="overlay-innertext">검색 결과를 추가적으로 살펴보면서, 앞선 단계와 같이 팩트체크할 내용에 대한 근거와 추가 특징들을 찾아볼 수 있습니다. 가장 마지막 세 번째 검색 결과의 출처 텍스트("대한민국 청와대")를 클릭해 보세요.</div>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 		</div>
 		break;
 		case 8:
@@ -478,6 +482,7 @@ switch(this.state.tutorialStep){
 			</div>
 			<div className="overlay-innertext">팩트 여부를 결정하는 데 있어 핵심적인 증거를 찾을 경우, 팩트 게이지가 움직입니다. 팩트 게이지를 참고하면서 충분히 단서를 탐색하고, 모두 끝난 후에 마무리 단계로 넘어가 주세요. 단서마다 증거가 가지고 있는 힘이 다르기 때문에, 팩트 게이지를 움직이는 정도가 다를 수 있습니다.</div>
 			<button className="overlay-nextButton" onClick={()=>{this.proceedTutorial()}}>다음 단계</button>
+			<button className="overlay-skipButton" onClick={()=>this.PlayGame()}>튜토리얼 스킵하기</button>
 		</div>
 		break;
 		case 9:
